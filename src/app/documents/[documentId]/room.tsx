756b25939eb580556bfeda6f20592d5b29468d64
michalwygonik <wygonikmichal@gmail.com>
2025-01-20 14:41:38 +0100

    add margin ruler functionality

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -55,7 +55,10 @@ export function Room({ children }: { children: ReactNode }) {
       }}
       resolveRoomsInfo={() => []}
     >
-      <RoomProvider id={params.documentId as string}>
+      <RoomProvider
+        id={params.documentId as string}
+        initialStorage={{ leftMargin: 56, rightMargin: 56 }}
+      >
         <ClientSideSuspense
           fallback={<FullscreenLoader label="Room loading..." />}
         >
